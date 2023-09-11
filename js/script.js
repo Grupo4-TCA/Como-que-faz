90a9f7362937d2948a2f67bf8f677d2ace5a1f3d
laismiharu <rm87271@estudante.fieb.edu.br>
2023-09-11 16:54:14 -0300

    lais

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 
     // funcao da conta
-function opcao(evt, nomeopcao) {
-    var i, tabcontent, tablinks;
-    
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    document.getElementById(nomeopcao).style.display = "block";
-    evt.currentTarget.className += " active";
+function opcao(evt, tab) {
+  var i;
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  var tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
+document.getElementById("Perfil").style.display = "block";
+document.getElementsByClassName("tablinks")[0].classList.add("active");
+
 
     // funcao da tabela dentro de conta
   function opiniao(evt, opiniaoperfil) {
@@ -35,20 +35,31 @@ function opcao(evt, nomeopcao) {
     evt.currentTarget.className += " active";
   }
 
+  
   //funcao da tabela entro de perfil
-  function piniquicao(evt, postagensopcao) {
-    var i, tabcontentfiltro, tablinksfiltro;
+  function openTab(tabName) {
+    var i;
+    var tabcontent = document.getElementsByClassName("tabcontent-filtro");
+    var tablinks = document.getElementsByClassName("tablinks-filtro");
   
-    tabcontentfiltro = document.getElementsByClassName("tabcontent-filtro");
-    for (i = 0; i < tabcontentfiltro.length; i++) {
-      tabcontentfiltro[i].style.display = "none";
+    // Esconda todos os conteúdos das abas
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
   
-    tablinksfiltro = document.getElementsByClassName("tablinks-filtro");
-    for (i = 0; i < tablinksfiltro.length; i++) {
-      tablinksfiltro[i].className = tablinksfiltro[i].className.replace(" active", "");
+    // Remova a classe "active" de todos os botões das abas
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
     }
   
-    document.getElementById(postagensopcao).style.display = "block";
-    evt.currentTarget.className += " active";
+    // Exiba o conteúdo da aba selecionada
+    document.getElementById(tabName).style.display = "block";
+  
+    // Adicione a classe "active" ao botão da aba selecionada
+    event.currentTarget.classList.add("active");
   }
+  
+  // Exiba a primeira aba como ativa por padrão
+  document.getElementById("Recentes").style.display = "block";
+  document.getElementsByClassName("tablinks-filtro")[0].classList.add("active");
+  
