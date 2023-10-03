1925c8f60e13bfaf6e73eaa6a716378bb64eeaaa
Your Name <you@example.com>
2023-10-03 17:04:39 -0300

    biel

--- js/script-catego.js
+++ js/script-catego.js
@@ -1,6 +1,8 @@
 const fcutodasElList = document.querySelectorAll('.filtrocu button');
 
 fcutodasElList.forEach(Option =>{
+    Option.setAttribute("class","")
+    console.log (Option)
 Option.addEventListener('click', () =>{
 Option.classList.add('clicado');
 });
