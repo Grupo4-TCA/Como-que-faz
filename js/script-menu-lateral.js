82de48d27706bf5c0c0d4ce50cc1570101737b9a
Your Name <you@example.com>
2023-10-04 20:01:18 -0300

    Sofiiii (finalização do cabeçalho logado)

---
+++ js/script-menu-lateral.js
@@ -0,0 +1,16 @@
+function AbrirMenuLateral() {
+    var x = document.getElementById("user-menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function FecharMenuLateral() {
+    var x = document.getElementById("user-menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
