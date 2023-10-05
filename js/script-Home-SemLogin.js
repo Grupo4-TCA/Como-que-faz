bdb6e958ea401c3631498a6ef60b7025354e3da7
Your Name <you@example.com>
2023-10-05 20:00:18 -0300

    sofiaa

--- js/script-Home-SemLogin.js
+++ js/script-Home-SemLogin.js
@@ -1,15 +1,28 @@
 let count = 1;
-document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-    nextImage();
-}, 200)
-
-function nextImage(){
+function nextImage() {
     count++;
-    if(count>3){
+    if (count > 3) {
         count = 1;
     }
+    document.getElementById("radio" + count).checked = true;
 
-    document.getElementById("radio"+count).checked = true;
+    // Defina o background apenas para o botão correspondente
+    const currentBtn = document.querySelector(".manual-btn" + count);
+    currentBtn.style.background = "#cb218c";
+
+    // Remova o background dos outros botões manuais
+    for (let i = 1; i <= 3; i++) {
+        if (i !== count) {
+            const otherBtn = document.querySelector(".manual-btn" + i);
+            otherBtn.style.background = "white";
+        }
+    }
 }
+
+// Inicie o carrossel automaticamente após o carregamento da página
+window.addEventListener("load", function () {
+    setInterval(nextImage, 3000);
+    document.getElementById("radio1").checked = true;
+    document.querySelector(".manual-btn1").style.background = "#cb218c";
+});
