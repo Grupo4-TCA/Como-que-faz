11b788a20f3fc63d354dd07795f51abc9922d4f0
Your Name <you@example.com>
2023-10-15 15:35:49 -0300

    sofia

---
+++ js/script-header.js
@@ -0,0 +1,16 @@
+        
+        function showDiv1() {
+            document.getElementById("hide-hover-span").style.display = "block";
+        }
+
+        function hideDiv1() {
+            document.getElementById("hide-hover-span").style.display = "none";
+        }
+
+        function showDiv2() {
+            document.getElementById("hide-hover-search").style.display = "block";
+        }
+
+        function hideDiv2() {
+            document.getElementById("hide-hover-search").style.display = "none";
+        }
