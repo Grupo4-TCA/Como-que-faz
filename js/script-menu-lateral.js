3e92e666f37f9c03162ba4c1a90209976793a502
Your Name <you@example.com>
2023-10-15 19:28:11 -0300

    sofia

--- js/script-menu-lateral.js
+++ js/script-menu-lateral.js
@@ -1,16 +1,22 @@
 function AbrirMenuLateral() {
     var x = document.getElementById("user-menu");
+    var y = document.getElementById("corpo");
     if (x.style.display === "none") {
-      x.style.display = "block";
+      x.style.display = "block";  
+      y.style.display = "none";  
     } else {
-      x.style.display = "none";
+      x.style.display = "none"; 
+      y.style.display = "block"; 
     }
   }
   function FecharMenuLateral() {
-    var x = document.getElementById("user-menu");
+    var x = document.getElementById("user-menu");    
+    var y = document.getElementById("corpo");
     if (x.style.display === "none") {
-      x.style.display = "block";
+      x.style.display = "block"; 
+      y.style.display = "none";  
     } else {
       x.style.display = "none";
+      y.style.display = "block"; 
     }
   }
