6fe58a22cdfb9cbb407c18884369d3277520e14f
Your Name <you@example.com>
2023-10-20 17:02:42 -0300

    biel

--- js/script-catego.js
+++ js/script-catego.js
@@ -54,3 +54,19 @@ function longPress() {
     timmyLong = setTimeout(resetButtons.bing(div), 1000);
 
 }
+//filtro subcategorias--------------------------------------------------------------//
+function category(c){
+    var item = document.getElementById('item-'+c).innerHTML;
+    document.getElementsByTagName('input')[0];
+}
+function dropdown(p){
+    var e = document.getElementsByClassName('dropDown')[0];
+    var d = ['block','none'];
+    e.style.display = d(p);
+
+    var t = ['0px', '0px-10px'];
+
+    setTimeout(function(){
+        e.style.transform = 'translate('+t[p]+')';
+    },0);
+}
