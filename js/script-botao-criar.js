dfdc409ebb9f6f994744449f19f5fa8a0bbe31eb
Your Name <you@example.com>
2023-10-19 22:53:51 -0300

    sofia

--- js/script-botao-criar.js
+++ js/script-botao-criar.js
@@ -51,3 +51,35 @@ function navigateToPage(page) {
   window.location.href = page;
 }
 
+// TABS 
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// TABS LOGIN - FIM
+
+// ICON BUTTON
+
+$(document).ready(function () {
+  $(".floating-main").click(function () {
+    $('.floating-smoke').toggleClass('float-open');
+});
+});
