62abe3ae710601a7a262d822c29f5c231c5ea406
laismiharu <rm87271@estudante.fieb.edu.br>
2023-10-20 11:58:50 -0300

    lais

--- js/script-catego.js
+++ js/script-catego.js
@@ -13,18 +13,44 @@ limpar = () => {
     })    
 }
 
-
-
-var pressTimer = document.getElementById("longpress");
-
-$("a").mouseup(function(){
-  clearTimeout(pressTimer);
-  // Clear timeout
-  return false;
-}).mousedown(function(){
-  // Set timeout
-  pressTimer = window.setTimeout(function() {
-    pressTimer.style.display='block';
-  },1000);
-  return false; 
+//longpress-------------------------------------------------------------------------------------//
+document.addEventListener('DOMContentLoaded', () => {
+    addClickTouch();
 });
+
+let timmy = null;
+let timmyLong = null;
+const delay = 800;
+
+function addClickTouch(){
+    clearTimeout(timmy);
+
+    if('ontouchstar' in document.body) {
+        document.querySelectorAll('[data-long]').forEach((div) => {
+            div.addEventListener('touchstart', start, {
+                once:true,
+            });
+        });
+    }
+}
+
+function start(ev) {
+    ev.preventDefault();
+
+    let ev = ev.target.closest('[data-long]');
+    div.id = 'longpress';
+
+    timmy = setTimeout(longPress.bind(div), delay);
+
+    div.addEventListener('mouseup', addClickTouch);
+    div.addEventListener('touchcancel', addClickTouch);
+}
+
+function longPress() {
+    let div = this;
+    div.removeEventListener('mouseup', addClickTouch);
+    div.removeEventListener('touchcancel', addClickTouch);
+
+    timmyLong = setTimeout(resetButtons.bing(div), 1000);
+
+}
