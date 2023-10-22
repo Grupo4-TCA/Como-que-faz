56ba0639cd298298cb94d58e47685c1df36b2a92
laismiharu <rm87271@estudante.fieb.edu.br>
2023-10-22 16:31:29 -0300

    lais

--- js/script-catego.js
+++ js/script-catego.js
@@ -14,46 +14,35 @@ limpar = () => {
 }
 
 //longpress-------------------------------------------------------------------------------------//
-document.addEventListener('DOMContentLoaded', () => {
-    addClickTouch();
-});
+//post
+var div = document.getElementById('post');
+    var longPressDiv = document.getElementById('longpress');
+    var nomedopost = document.getElementById('descri-post');
+    var longpress;
 
-let timmy = null;
-let timmyLong = null;
-const delay = 800;
+    div.addEventListener('mousedown', startLongPress);
+    div.addEventListener('mouseup', clearLongPress);
+    div.addEventListener('touchstart', startLongPress);
+    div.addEventListener('touchend', clearLongPress);
 
-function addClickTouch(){
-    clearTimeout(timmy);
-
-    if('ontouchstar' in document.body) {
-        document.querySelectorAll('[data-long]').forEach((div) => {
-            div.addEventListener('touchstart', start, {
-                once:true,
-            });
-        });
+    function startLongPress() {
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "flex";
+            nomedopost.style.display = "none";
+        }, 1000);
     }
-}
 
-function start(ev) {
-    ev.preventDefault();
+    function clearLongPress() {
+        clearTimeout(longpress);
+        longPressDiv.style.display = "none";
+        nomedopost.style.display = "block";
+    }
 
-    let ev = ev.target.closest('[data-long]');
-    div.id = 'longpress';
+    longPressDiv.style.display = "none";
 
-    timmy = setTimeout(longPress.bind(div), delay);
+//post-a
+//FIM longpress-------------------------------------------------------------------------------------//
 
-    div.addEventListener('mouseup', addClickTouch);
-    div.addEventListener('touchcancel', addClickTouch);
-}
-
-function longPress() {
-    let div = this;
-    div.removeEventListener('mouseup', addClickTouch);
-    div.removeEventListener('touchcancel', addClickTouch);
-
-    timmyLong = setTimeout(resetButtons.bing(div), 1000);
-
-}
 //filtro subcategorias--------------------------------------------------------------//
 function category(c){
     var item = document.getElementById('item-'+c).innerHTML;
