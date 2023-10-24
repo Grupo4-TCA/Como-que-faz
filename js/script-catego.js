4b0f23191559af4344312b645292b13be7977a12
laismiharu <rm87271@estudante.fieb.edu.br>
2023-10-24 11:32:30 -0300

    lais

--- js/script-catego.js
+++ js/script-catego.js
@@ -15,9 +15,10 @@ limpar = () => {
 
 //longpress-------------------------------------------------------------------------------------//
 //post
-var div = document.getElementById('post-dispensa');
-    var longPressDiv = document.getElementById('longpress');
-    var nomedopost = document.getElementById('descri-post');
+function configureLongPress(divId, longPressDivId, descriptionId) {
+    var div = document.getElementById(divId);
+    var longPressDiv = document.getElementById(longPressDivId);
+    var nomedopost = document.getElementById(descriptionId);
     var longpress;
 
     div.addEventListener('mousedown', startLongPress);
@@ -29,27 +30,75 @@ var div = document.getElementById('post-dispensa');
         e.preventDefault();
     });
 
-    // Impede o menu de contexto ao clicar com o botão direito do mouse
-    div.addEventListener('contextmenu', function(e) {
-        e.preventDefault();
+    document.addEventListener('contextmenu', function(e) {
+        if (e.target !== div) {
+            e.preventDefault();
+        }
     });
 
     function startLongPress() {
         longpress = setTimeout(function() {
             longPressDiv.style.display = "flex";
             nomedopost.style.display = "none";
-        }, 1000);
+        }, 700);
     }
 
     function clearLongPress() {
         clearTimeout(longpress);
-        longPressDiv.style.display = "none";
-        nomedopost.style.display = "block";
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "none";
+            nomedopost.style.display = "block";
+        }, 1500);
     }
 
     longPressDiv.style.display = "none";
+}
+configureLongPress('post-dispensa-fileira1', 'longpress-post-dispensa-fileira1', 'descri-post');
 
 //post-a
+function configureLongPress(divId, longPressDivId, descriptionId) {
+    var div = document.getElementById(divId);
+    var longPressDiv = document.getElementById(longPressDivId);
+    var nomedopost = document.getElementById(descriptionId);
+    var longpress;
+
+    div.addEventListener('mousedown', startLongPress);
+    div.addEventListener('mouseup', clearLongPress);
+    div.addEventListener('touchstart', startLongPress);
+    div.addEventListener('touchend', clearLongPress);
+
+    div.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('contextmenu', function(e) {
+        if (e.target !== div) {
+            e.preventDefault();
+        }
+    });
+
+    function startLongPress() {
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "flex";
+            nomedopost.style.display = "none";
+        }, 700);
+    }
+
+    function clearLongPress() {
+        clearTimeout(longpress);
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "none";
+            nomedopost.style.display = "block";
+        }, 1500);
+    }
+
+    longPressDiv.style.display = "none";
+}
+// Chame a função com os IDs específicos
+configureLongPress('post-bolin-fileira1', 'longpress-post-bolin-fileira1', 'descri-post-a');
+
+
+
 //FIM longpress-------------------------------------------------------------------------------------//
 
 //filtro subcategorias--------------------------------------------------------------//
