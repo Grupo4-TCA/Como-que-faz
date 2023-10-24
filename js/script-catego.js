5653fe852b4d89ae68b274a7c89f082373cd19e3
Your Name <you@example.com>
2023-10-24 15:56:56 -0300

    lais

--- js/script-catego.js
+++ js/script-catego.js
@@ -14,7 +14,7 @@ limpar = () => {
 }
 
 //longpress-------------------------------------------------------------------------------------//
-//post
+//1 POST DISPENSA - FILEIRA 1
 function configureLongPress(divId, longPressDivId, descriptionId) {
     var div = document.getElementById(divId);
     var longPressDiv = document.getElementById(longPressDivId);
@@ -54,8 +54,9 @@ function configureLongPress(divId, longPressDivId, descriptionId) {
     longPressDiv.style.display = "none";
 }
 configureLongPress('post-dispensa-fileira1', 'longpress-post-dispensa-fileira1', 'descri-post');
+// FINAL 1 POST DISPENSA - FILEIRA 1
 
-//post bolinho de chuva
+//2 POST BOLIN - FILEIRA 1
 function configureLongPress(divId, longPressDivId, descriptionId) {
     var div = document.getElementById(divId);
     var longPressDiv = document.getElementById(longPressDivId);
@@ -94,10 +95,134 @@ function configureLongPress(divId, longPressDivId, descriptionId) {
 
     longPressDiv.style.display = "none";
 }
-// Chame a função com os IDs específicos
 configureLongPress('post-bolin-fileira1', 'longpress-post-bolin-fileira1', 'descri-post-a');
+// FINAL 2 POST BOLIN - FILEIRA 1
 
+//3 POST CACHOS - FILEIRA 1
+function configureLongPress(divId, longPressDivId, descriptionId) {
+    var div = document.getElementById(divId);
+    var longPressDiv = document.getElementById(longPressDivId);
+    var nomedopost = document.getElementById(descriptionId);
+    var longpress;
 
+    div.addEventListener('mousedown', startLongPress);
+    div.addEventListener('mouseup', clearLongPress);
+    div.addEventListener('touchstart', startLongPress);
+    div.addEventListener('touchend', clearLongPress);
+
+    div.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('contextmenu', function(e) {
+        if (e.target !== div) {
+            e.preventDefault();
+        }
+    });
+
+    function startLongPress() {
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "flex";
+            nomedopost.style.display = "none";
+        }, 700);
+    }
+
+    function clearLongPress() {
+        clearTimeout(longpress);
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "none";
+            nomedopost.style.display = "block";
+        }, 1500);
+    }
+
+    longPressDiv.style.display = "none";
+}
+configureLongPress('post-cachos-fileira1', 'longpress-post-cachos-fileira1', 'descri-post-cachos-fileira1');
+//FINAL 3 POST CACHOS - FILEIRA 1
+
+//1 POST CACHOS - FILEIRA 2
+function configureLongPress(divId, longPressDivId, descriptionId) {
+    var div = document.getElementById(divId);
+    var longPressDiv = document.getElementById(longPressDivId);
+    var nomedopost = document.getElementById(descriptionId);
+    var longpress;
+
+    div.addEventListener('mousedown', startLongPress);
+    div.addEventListener('mouseup', clearLongPress);
+    div.addEventListener('touchstart', startLongPress);
+    div.addEventListener('touchend', clearLongPress);
+
+    div.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('contextmenu', function(e) {
+        if (e.target !== div) {
+            e.preventDefault();
+        }
+    });
+
+    function startLongPress() {
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "flex";
+            nomedopost.style.display = "none";
+        }, 700);
+    }
+
+    function clearLongPress() {
+        clearTimeout(longpress);
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "none";
+            nomedopost.style.display = "block";
+        }, 1500);
+    }
+
+    longPressDiv.style.display = "none";
+}
+configureLongPress('post-cachos-fileira2', 'longpress-post-cachos-fileira2', 'descri-post-cachos-fileira2');
+//1 POST CACHOS - FILEIRA 2
+
+//2 POST BOLO DE CENOURA - FILEIRA 2
+function configureLongPress(divId, longPressDivId, descriptionId) {
+    var div = document.getElementById(divId);
+    var longPressDiv = document.getElementById(longPressDivId);
+    var nomedopost = document.getElementById(descriptionId);
+    var longpress;
+
+    div.addEventListener('mousedown', startLongPress);
+    div.addEventListener('mouseup', clearLongPress);
+    div.addEventListener('touchstart', startLongPress);
+    div.addEventListener('touchend', clearLongPress);
+
+    div.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('contextmenu', function(e) {
+        if (e.target !== div) {
+            e.preventDefault();
+        }
+    });
+
+    function startLongPress() {
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "flex";
+            nomedopost.style.display = "none";
+        }, 700);
+    }
+
+    function clearLongPress() {
+        clearTimeout(longpress);
+        longpress = setTimeout(function() {
+            longPressDiv.style.display = "none";
+            nomedopost.style.display = "block";
+        }, 1500);
+    }
+
+    longPressDiv.style.display = "none";
+}
+configureLongPress('post-cachos-fileira1', 'longpress-post-cachos-fileira1', 'descri-post-b');
+// FINAL 2 POST BOLO DE CENOURA - FILEIRA 2
 
 //FIM longpress-------------------------------------------------------------------------------------//
 
