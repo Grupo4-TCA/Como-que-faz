2963ec982f8f8c2d76133189dbb6673ef11547db
Your Name <you@example.com>
2023-10-31 11:35:16 -0300

    sofia

--- js/script-perfil.js
+++ js/script-perfil.js
@@ -32,3 +32,23 @@ function VisiInteracoes() {
         z.style.display = "block";
     }
 }
+
+function EditarPerfil(){
+    var popup = document.getElementById("editar-perfil-popup");
+    var fundo = document.getElementById("fundo-editar-perfil");
+
+    if (popup.style.display === "none"){
+        popup.style.display = "block";
+        fundo.style.display = "block";
+    }
+}
+
+function SairEditarPerfil(){
+    var popup = document.getElementById("editar-perfil-popup");
+    var fundo = document.getElementById("fundo-editar-perfil");
+
+    if (popup.style.display === "block"){
+        popup.style.display = "none";
+        fundo.style.display = "none";
+    }
+}
