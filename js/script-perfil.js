4d2b01194c6e26b7cae759f7886e126b85e63b1d
Your Name <you@example.com>
2023-11-03 21:55:38 -0300

    sofia

--- js/script-perfil.js
+++ js/script-perfil.js
@@ -56,19 +56,23 @@ function SairEditarPerfil(){
 function EditarPerfilMob(){
     var popup = document.getElementById("editar-perfil-popup-mob");
     var fundo = document.getElementById("fundo-editar-perfil-mob");
+    var categorias = document.getElementById("topcs");
 
     if (popup.style.display === "none"){
         popup.style.display = "block";
         fundo.style.display = "block";
+        categorias.style.display = "none";
     }
 }
 
 function SairEditarPerfilMob(){
     var popup = document.getElementById("editar-perfil-popup-mob");
     var fundo = document.getElementById("fundo-editar-perfil-mob");
+    var categorias = document.getElementById("topcs");
 
     if (popup.style.display === "block"){
         popup.style.display = "none";
         fundo.style.display = "none";
+        categorias.style.display = "flex";
     }
 }
